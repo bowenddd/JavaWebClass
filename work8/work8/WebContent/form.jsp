1f463c397ccef562a512a183f06c0749eab9f231
Bowenddd <925634263@qq.com>
2018-11-29 16:38:54 +0800

    work8

---
+++ work8/work8/WebContent/form.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=gb2312"
+    pageEncoding="gb2312"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action = "DealWithServlet" method = "post">
+		请输入学生资料：
+		<input type = "text" name = "stuname"><br>
+		<input type = "submit" value = "查询">
+	</form>
+</body>
+</html>
---
+++ work8/work8/src/work8/DealWithServlet.java
@@ -0,0 +1,41 @@
+package work8;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DealWithServlet
+ */
+@WebServlet("/DealWithServlet")
+public class DealWithServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DealWithServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("stuname");
+		System.out.println(name);
+	}
+
+}
